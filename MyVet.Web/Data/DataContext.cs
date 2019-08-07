16d714ea5e011da563433284d509d3d8023943fd
ShenLong03 <donovanjarquin@amarusystems.com>
2019-08-07 10:52:38 -0600

    Base de datos completa

---
+++ MyVet.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿namespace MyVet.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MyVet.Web.Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyVet.Web.Data;
 
 namespace MyVet.Web
 {
@@ -30,6 +32,10 @@ namespace MyVet.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
